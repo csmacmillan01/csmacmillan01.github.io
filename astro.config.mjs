810f494ddec98fedf301d26b032f85358d5b01bb
Cole MacMillan <csmacmillan@wm.edu>
2026-02-26 16:36:27 -0500

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://csmacmillan01.github.io',
+  output: 'static',
+});
